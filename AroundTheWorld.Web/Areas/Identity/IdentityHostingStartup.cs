185fbe456764cdd81daa9d8952bbae85c668d59e
andrada.marin5 <andrada.marin5@gmail.com>
2020-08-23 21:49:40 +0300

    Change Login, Register, Forgot password pages

---
+++ AroundTheWorld.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AroundTheWorld.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AroundTheWorld.Web.Areas.Identity.IdentityHostingStartup))]
+namespace AroundTheWorld.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
